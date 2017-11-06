5f1b69ed120ba1e8f21ce02dadd0c81edf26d815
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-11-05 22:21:55 -0800

    adds merch page; adds functionality to css

--- sales.js
+++ sales.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '8pm',];
-const stores = ['PDX Airport', 'Pioneer', 'Powell\'s', 'St. John\'s', 'Waterfront'];
 
 // constructor function to build store and its data (function declaration)
 function Store(name, min, max, avgSold) {
@@ -21,7 +20,6 @@ Store.prototype.renderCookiesSold = function() {
         const hour = hours[i];
         const custPerHour = getRandomIntInclusive(this.min, this.max);
         const cookiesPerHour = Math.floor(custPerHour * this.avgSold);
-        const salesByDay = sum(cookiesPerHour[i]);
         const oneHour = {
             hour: hour,
             cookiesSold: cookiesPerHour
@@ -30,9 +28,15 @@ Store.prototype.renderCookiesSold = function() {
     }
 };
 
+const airport = new Store('PDX Airport', 23, 65, 6.3);
+const pioneer = new Store('Pioneer Square', 3, 24, 1.2);
+const powells = new Store('Powell\s', 11, 38, 3.7);
+const stjohns = new Store('St. John\s', 20, 38, 2.3);
+const waterfront = new Store('Waterfront', 2, 16, 4.6);
 
-Store.prototype.buildTable = function() {
-    const stores = document.getElementById('stores-list');
+//const stores = ['PDX Airport', 'Pioneer', 'Powell\'s', 'St. John\'s', 'Waterfront'];
+
+Table.prototype.buildTable = function() {
     // table construction
     const tableRow = document.createElement('TR'); // creates a row for each store
     const rowHeader = document.createElement('TH'); // creates a column for the store name
@@ -48,20 +52,16 @@ Store.prototype.buildTable = function() {
     }
     // add totals to end or row
     const totalSales = document.createElement('TD'); // creates place for total sales data
-    tableRow.appendChild(totalSales); 
+    tableRow.appendChild(totalSales);
     totalSales.textContent = this.salesByDay;
-}
+};
 
-console.log('table row = ', tableRow);
-this.row = tableRow;
+airport.renderCookiesSold();
+pioneer.renderCookiesSold();
+powells.renderCookiesSold();
+stjohns.renderCookiesSold();
+waterfront.renderCookiesSold();
 
-
-const airport = new Store('PDX Airport', 23, 65, 6.3);
-const pioneer = new Store('Pioneer Square', 3, 24, 1.2);
-const powells = new Store('Powell\s', 11, 38, 3.7);
-const stjohns = new Store('St. John\s', 20, 38, 2.3);
-const waterfront = new Store('Waterfront', 2, 16, 4.6);
-//
 // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
